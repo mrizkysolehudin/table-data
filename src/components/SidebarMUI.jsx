a0db090a0d2236f06b4da5bb9c43b82d67f813ce
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-06-08 14:00:17 +0700

    add fitur search dan data table in sidebar

--- src/components/SidebarMUI.jsx
+++ src/components/SidebarMUI.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Drawer from "@mui/material/Drawer";
 import DensityMediumIcon from "@mui/icons-material/DensityMedium";
 import avatar from "../assets/avatar.png";
@@ -6,9 +6,28 @@ import searchIcon from "../assets/searchIcon.svg";
 import DashboardOutlinedIcon from "@mui/icons-material/DashboardOutlined";
 import PowerOffIcon from "@mui/icons-material/PowerSettingsNew";
 import supportIcon from "../assets/supportIcon.svg";
+import CloseIcon from "@mui/icons-material/Close";
+import { useRecoilValue } from "recoil";
+import { dataUsersState } from "../recoil/atoms";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
 	const drawerWidth = 250;
+	const [openDataTable, setOpenDataTable] = useState(false);
+	const dataUsers = useRecoilValue(dataUsersState);
+
+	const [openSearch, setOpenSearch] = useState(false);
+	const [searchTerm, setSearchTerm] = useState("");
+
+	const searchData = dataUsers?.filter((data) => {
+		return (
+			data?.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+			data?.lastName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+			(data?.firstName + " " + data?.lastName)
+				.toLowerCase()
+				.includes(searchTerm.toLowerCase())
+		);
+	});
 
 	return (
 		<div id="sidebar">
@@ -98,6 +117,10 @@ const Sidebar = () => {
 								<input
 									type="text"
 									placeholder="Search"
+									value={searchTerm}
+									onChange={(e) =>
+										setSearchTerm(e.target.value)
+									}
 									style={{
 										backgroundColor: "#F1F2F6",
 
@@ -111,19 +134,69 @@ const Sidebar = () => {
 									}}
 								/>
 
-								<button
-									style={{
-										padding: "6px 4px",
-										borderRadius: 2,
-									}}>
-									<DensityMediumIcon
+								{openSearch ? (
+									<button
+										onClick={() => setOpenSearch(false)}
 										style={{
-											color: "#757D8A",
-											fontWeight: 100,
-										}}
-									/>
-								</button>
+											padding: "6px 4px",
+											borderRadius: 2,
+										}}>
+										<CloseIcon
+											style={{
+												color: "#757D8A",
+												fontWeight: 100,
+											}}
+										/>
+									</button>
+								) : (
+									<button
+										onClick={() => setOpenSearch(true)}
+										style={{
+											padding: "6px 4px",
+											borderRadius: 2,
+										}}>
+										<DensityMediumIcon
+											style={{
+												color: "#757D8A",
+												fontWeight: 100,
+											}}
+										/>
+									</button>
+								)}
 							</div>
+
+							{openSearch && (
+								<article
+									style={{
+										padding: "0 20px",
+										backgroundColor: "white",
+										marginBottom: "10px",
+										borderRadius: 10,
+									}}>
+									<div
+										style={{
+											display: "flex",
+											flexDirection: "column",
+											marginTop: -15,
+										}}>
+										{searchTerm
+											? searchData?.map((user, index) => (
+													<Link
+														to={`/edit/${user?._id}`}
+														style={{
+															color: "#757474",
+															textDecoration:
+																"none",
+															padding: "8px 0",
+														}}>
+														{index + 1}.{" "}
+														{user.firstName}
+													</Link>
+											  ))
+											: ""}
+									</div>
+								</article>
+							)}
 
 							<div
 								style={{
@@ -161,20 +234,59 @@ const Sidebar = () => {
 									</p>
 								</div>
 
-								<button
-									style={{
-										padding: "6px 4px",
-										borderRadius: 2,
-									}}>
-									<DensityMediumIcon
+								{openDataTable ? (
+									<button
+										onClick={() => setOpenDataTable(false)}
 										style={{
-											color: "#757D8A",
-											fontWeight: 100,
-										}}
-									/>
-								</button>
+											padding: "6px 4px",
+											borderRadius: 2,
+										}}>
+										<CloseIcon
+											style={{
+												color: "#757D8A",
+												fontWeight: 100,
+											}}
+										/>
+									</button>
+								) : (
+									<button
+										onClick={() => setOpenDataTable(true)}
+										style={{
+											padding: "6px 4px",
+											borderRadius: 2,
+										}}>
+										<DensityMediumIcon
+											style={{
+												color: "#757D8A",
+												fontWeight: 100,
+											}}
+										/>
+									</button>
+								)}
 							</div>
 						</section>
+
+						{openDataTable && (
+							<article style={{ margin: "0 30px " }}>
+								<div
+									style={{
+										display: "flex",
+										flexDirection: "column",
+										marginTop: 9,
+									}}>
+									{dataUsers.map((user, index) => (
+										<Link
+											to={`/edit/${user?._id}`}
+											style={{
+												color: "#757474",
+												textDecoration: "none",
+											}}>
+											{index + 1}. {user.firstName}
+										</Link>
+									))}
+								</div>
+							</article>
+						)}
 					</div>
 
 					<div style={{ textAlign: "start", margin: "0 20px 40px" }}>
